c587c9473fd8fba1d82e50a8e6f4f7d6b23f148a
Dennis Nichols <dgnichols22@gmail.com>
2022-12-24 14:05:10 -0600

    first commit, setup structure

---
+++ python/docs/linked_list_insertions/tests/test_linked_list_insertions.py
@@ -0,0 +1,18 @@
+import pytest
+from linked_list_insertions import Node, LinkedList
+
+# Can successfully add a node to the end of the linked list
+# Can successfully add multiple nodes to the end of a linked list
+# Can successfully insert a node before a node located i the middle of a linked list
+# Can successfully insert a node before the first node of a linked list
+# Can successfully insert after a node in the middle of the linked list
+# Can successfully insert a node after the last node of the linked list
+
+
+def test_node_proof_of_life():
+    assert Node
+
+
+def test_ll_proof_of_life():
+    assert LinkedList
+
