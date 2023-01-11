dfa44631511b521ee7c1f7d0da57f4383f032a41
Dennis Nichols <dgnichols22@gmail.com>
2023-01-11 18:11:53 -0500

    made function, passing tests

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,40 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree, Node
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(tree):
+    """
+    returns a "shallow" copy of the current tree as a new tree with the fizzbuzz algorithm applied to each original node's value.
+    """
+    if not tree.root:
+        return KaryTree()
+
+    def fizz_buzz(value):
+        if type(value) != int or value < 0:
+            return 'not a positive integer'
+        elif value % 15 == 0:
+            return 'FizzBuzz'
+        elif value % 3 == 0:
+            return 'Fizz'
+        elif value % 5 == 0:
+            return 'Buzz'
+        else:
+            return str(value)
+
+    def walk(source_root, clone_root):
+        """
+        recursive method to clone Nodes
+        """
+        if not source_root:
+            return
+
+        for source_child in source_root.children:
+            clone_child = Node(fizz_buzz(source_child.value))
+            clone_root.children.append(clone_child)
+            walk(source_child, clone_child)
+
+    clone_tree = KaryTree()
+
+    clone_tree.root = Node(fizz_buzz(tree.root.value))
+    walk(tree.root, clone_tree.root)
+
+    return clone_tree
