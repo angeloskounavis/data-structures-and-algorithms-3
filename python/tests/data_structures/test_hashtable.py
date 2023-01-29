4efd02dc081d23b475375675a9a5e801331dd6c7
Dennis Nichols <dgnichols22@gmail.com>
2023-01-29 13:50:36 -0600

    updated hashtable tests

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,7 +6,7 @@ def test_exists():
     assert Hashtable
 
 
-def test_get_apple():
+def test_get_apple(): # tests get and set
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
     actual = hashtable.get("apple")
@@ -33,3 +33,42 @@ def test_internals():
     expected = [[["silent", True]], [["listen", "to me"]], [["ahmad", 30]]]
 
     assert actual == expected
+
+
+def test_get_null():# tests returning null for a value not in table
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    actual = hashtable.get("orange")
+    expected = None
+    assert actual == expected
+
+
+def test_keys_method():
+    hashtable = Hashtable(1024)
+    hashtable.set("ahmad", 30)
+    hashtable.set("silent", True)
+    hashtable.set("listen", "to me")
+
+    expected = ['ahmad', 'listen', 'silent']
+    actual = hashtable.keys()
+    assert actual == expected
+
+
+def test_collision():
+    hashtable = Hashtable(1)
+    hashtable.set("ahmad", 30)
+    hashtable.set("silent", True)
+    hashtable.set("listen", "to me")
+    print(hashtable)
+
+    actual = hashtable.get('silent')
+    expected = True
+
+    assert actual == expected
+
+
+def test_hash_in_range():
+    hashtable = Hashtable(1024)
+    actual = hashtable.hash('So long and thanks for all the fish')
+    assert actual in range(1024)
+
